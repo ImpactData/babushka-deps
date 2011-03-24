1d6f564c3bcd066ba247f3b1a8e0f90861fb24bf
Mark Magnus <mark@impactdata.com.au>
2011-03-25 10:27:34 +1100

    postgres support

--- postgres.rb
+++ postgres.rb
@@ -1,49 +1,15 @@
-#hacked from benhoskings:postgres creds
-dep 'existing postgres db' do
-  requires 'postgres access'
-  met? {
-    !shell("psql -l") {|shell|
-      shell.stdout.split("\n").grep(/^\s*#{var :db_name}\s+\|/)
-    }.empty?
-  }
-  meet {
-    shell "createdb -O '#{var :username}' '#{var :db_name}'"
-  }
-end
-
-dep 'existing data' do
-  requires 'existing db'
-  met? {
-    shell("psql #{var(:db_name)} -c '\\d'").scan(/\((\d+) rows?\)/).flatten.first.tap {|rows|
-      if rows && rows.to_i > 0
-        log "There are already #{rows} tables."
-      else
-        unmeetable <<-MSG
-That database is empty. Load a database dump with:
-$ cat #{var(:db_name)} | ssh #{var(:username)}@#{var(:domain)} 'psql #{var(:db_name)}'
-        MSG
-      end
-    }
-  }
-end
-
-dep 'pg.gem' do
-  requires 'postgres.managed'
-  provides []
-end
-
-dep 'postgres access' do
+dep 'postgres.native.access' do
   requires 'postgres.managed', 'user exists'
   met? { !sudo("echo '\\du' | #{which 'psql'}", :as => 'postgres').split("\n").grep(/^\W*\b#{var :username}\b/).empty? }
   meet { sudo "createuser -SdR #{var :username}", :as => 'postgres' }
 end
 
-dep 'pgadmin3.managed' do
+dep 'pgadmin3.native' do
     requires 'postgres.managed'
     meet{sudo("apt-get install pgadmin3")}
 end
 
-dep 'postgres.managed' do
+dep 'postgres.native' do
   meet { sudo("apt-get install postgresql postgresql-client libpq-dev") }
 end
 
