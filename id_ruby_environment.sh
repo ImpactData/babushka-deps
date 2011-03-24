5d257d129b6f72a473e88dbd18fbd3441dfa1c8f
Mark Magnus <mark@impactdata.com.au>
2011-03-24 15:39:05 +1100

    second try

--- id_ruby_environment.sh
+++ id_ruby_environment.sh
@@ -8,6 +8,7 @@
 sudo apt-get install ruby ri rdoc irb ri1.8 ruby1.8-dev libzlib-ruby zlib1g
 bash -c "`wget -O - babushka.me/up`"
 babushka 'core:curl.managed'
+#git
 babushka 'core:git'
 babushka source -a impactdata git://github.com/MarkMagnus/squawk_babushka_dep.git
 
@@ -15,16 +16,17 @@ babushka source -a impactdata git://github.com/MarkMagnus/squawk_babushka_dep.gi
 babushka 'impactdata:1.8.7.ee.rvm'
 
 #Rails 2.3.9
-
-#zsh # suggested - oh-my-zsh has useful stuff (aliases, colouring, etc.) for git
-#mongodb postgres pgadmin3
-#git
-#ruby-dev libxml2 libxml2-dev libsasl2-dev libxslt-dev libxml2-dev libpq-dev imagemagick libmagickcore-dev libmagickwand-dev # Needed to build certain gems
-
 #gems:
 #bundler
 #heroku
 #thin # suggested - faster Rack server
+babushka 'impactdata:2.3.9.rails.rb'
+
+#zsh # suggested - oh-my-zsh has useful stuff (aliases, colouring, etc.) for git
+#mongodb postgres pgadmin3
+
+#ruby-dev libxml2 libxml2-dev libsasl2-dev libxslt-dev libxml2-dev libpq-dev imagemagick libmagickcore-dev libmagickwand-dev # Needed to build certain gems
+
 
 #then:
 #git config --global user.name "Your GitHub ID"
