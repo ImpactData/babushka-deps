34869b0c97d4b61fe12f4108e1ce7356d4700ac8
Mark Magnus <mark@impactdata.com.au>
2011-03-25 10:45:46 +1100

    postgres support

--- postgres.rb
+++ postgres.rb
@@ -1,3 +1,9 @@
+meta :aptget do
+  def aptget args
+    sudo("apt-get install #{args}")
+  end
+end
+
 dep 'postgres.native.access' do
   requires 'postgres.native', 'user exists'
   met? { !sudo("echo '\\du' | #{which 'psql'}", :as => 'postgres').split("\n").grep(/^\W*\b#{var :username}\b/).empty? }
@@ -6,16 +12,12 @@ end
 
 dep 'pgadmin3.native' do
     requires 'postgres.native'
-    meet{sudo("apt-get install pgadmin3")}
-    met? {
-        shell('pgadmin3 --version').include?('pgadmin3')
-    }
+    meet{aptget("pgadmin3")}
+    met? { shell('pgadmin3 --version')['pgadmin3'] }
 end
 
 dep 'postgres.native' do
-    meet { sudo("apt-get install postgresql postgresql-client libpq-dev") }
-    met? {
-        shell('psql --version').include?('psql')
-    }
+    meet { aptget("postgresql postgresql-client libpq-dev") }
+    met? { shell('psql --version')['psql'] }
 end
 
