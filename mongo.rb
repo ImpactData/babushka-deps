c660a33067a4ea2e77b818f8b4b183382d270194
Mark Magnus <mark@impactdata.com.au>
2011-03-25 15:14:08 +1100

    mongo support

---
+++ mongo.rb
@@ -0,0 +1,13 @@
+meta :aptget do
+  def aptget args
+    sudo("apt-get install #{args}")
+  end
+end
+
+dep 'mongo.aptget' do
+    meet{ aptget('mongodb')['--fix-missing']}
+    met?{ shell('mongo --version') ~= /.*MongoDB.*/ }
+    after{
+        aptget('mongodb')['--fix-missing']
+    }
+end 
