0ac3b94b49684122cdd7d2774fe0852cf332ef07
Mark Magnus <mark@impactdata.com.au>
2011-03-25 11:03:24 +1100

    postgres support

--- postgres.rb
+++ postgres.rb
@@ -5,32 +5,20 @@ meta :aptget do
   end
 end
 
-meta :pgadmin3 do
-  def pgadmin3 args
-    shell("pgadmin3 #{args}")
-  end
-end
-
-meta :psql do
-  def psql args
-    shell("psql #{args}")
-  end
-end
-
 dep 'postgres.native.access' do
   requires 'postgres.native', 'user exists'
   met? { !sudo("echo '\\du' | #{which 'psql'}", :as => 'postgres').split("\n").grep(/^\W*\b#{var :username}\b/).empty? }
   meet { sudo "createuser -SdR #{var :username}", :as => 'postgres' }
 end
 
-dep 'pgadmin3.native' do
-    requires 'postgres.native'
+dep 'pgadmin3.aptget' do
+    requires 'postgres.aptget'
     meet{ aptget("pgadmin3")}
-    met?{ pgadmin3('--version') =~ /.*pgadmin3.*/ }
+    met?{ shell('pgadmin3 --version') =~ /.*pgadmin3.*/ }
 end
 
-dep 'postgres.native' do
+dep 'postgres.aptget' do
     meet { aptget("postgresql postgresql-client libpq-dev") }
-    met? { psql('--version') =~ /.*psql.*/  }
+    met? { shell('psql --version') =~ /.*psql.*/  }
 end
 
