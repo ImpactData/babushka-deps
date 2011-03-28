61f0801d0a094151c2679e279babe1cc325794d1
Mark Magnus <mark@impactdata.com.au>
2011-03-28 14:13:47 +1100

    deploy rails app

--- 2.3.9.rails.rb
+++ 2.3.9.rails.rb
@@ -1,6 +1,6 @@
 meta :gem do
   def gem args
-    sudo "gem #{args}", :log => args['install']
+    sudo "~/.rvm/rubies/ree-1.8.7-head/bin/gem list #{args}", :log => args['install']
   end
 end
 
@@ -15,34 +15,34 @@ dep '2.3.9.rails' do
 end
 
 dep '2.3.9.rails.gem' do
-    met? { login_shell('gem list rails')['rails (2.3.9)'] }
+    met? { gem('list rails')['rails (2.3.9)'] }
     meet { gem('install rails --VERSION=2.3.9 --include-dependencies') }
 end 
 
 dep 'postgres.gem' do
   requires 'postgres.access'
-  met? { login_shell('gem list postgres')['postgres'] }
+  met? { gem('list postgres')['postgres'] }
   meet { gem('install postgres') }
 end
 
 dep 'bundler.gem' do
     requires 'rubygems_update.gem'
-    met? { login_shell('gem list bundler')['bundler (0.9.7)'] }
-    meet { gem('install bundler --VERSION=0.9.7') }
+    met? { gem('list bundler')['bundler'] }
+    meet { gem('install bundler') }
 end
 
 dep 'rubygems_update.gem' do
-    met? { login_shell('gem list rubygems-update')['rubygems-update'] }
+    met? { gem('list rubygems-update')['rubygems-update'] }
     meet { gem('install rubygems-update') }
 end
 
 dep 'heroku.gem' do
-    met? { login_shell('gem list heroku')['heroku'] }
+    met? { gem('list heroku')['heroku'] }
     meet { gem('install heroku') }
 end
 
 dep 'thin.gem' do
-    met? { login_shell('gem list thin')['thin'] }
+    met? { gem('list thin')['thin'] }
     meet { gem('install thin') }
 end
 
