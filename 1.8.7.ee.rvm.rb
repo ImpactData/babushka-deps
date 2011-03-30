fc90594bcec23ff9722af3288337ba136a37d7bd
Mark Magnus <mark@impactdata.com.au>
2011-03-31 09:51:24 +1100

    fix rvm install

--- 1.8.7.ee.rvm.rb
+++ 1.8.7.ee.rvm.rb
@@ -11,6 +11,9 @@ dep 'append_bashrc' do
 	} 
 	meet {
 		shell("echo '[[ -s \"$HOME/.rvm/scripts/rvm\" ]] && . \"$HOME/.rvm/scripts/rvm\"' >> .bashrc")
+		log "Will need to restart bash terminal"
+		log "Run $source ~/.bashrc"
+		log "For this to take effect"
 	}
 end
 
