bd9a1e8e563aea47c0e594aefc1e1428c0fab851
Mark Magnus <mark@impactdata.com.au>
2011-04-12 09:53:40 +1000

    changed apt-get conditoinals to be more generic

--- dependant_libs.rb
+++ dependant_libs.rb
@@ -5,57 +5,57 @@ end
 
 dep 'rubydev.aptget' do
     meet{ aptget('ruby-dev')}
-    met?{ aptget('ruby-dev') =~ /.*ruby-dev is already the newest version.*/ }
+    met?{ aptget('ruby-dev') =~ /.*is already the newest version.*/ }
 end 
 
 dep 'libxml2dev.aptget' do
     meet{ aptget('libxml2-dev')}
-    met?{ aptget('libxml2-dev') =~ /.*libxml2-dev is already the newest version.*/ }
+    met?{ aptget('libxml2-dev') =~ /.*is already the newest version.*/ }
 end 
 
 dep 'libsasl2dev.aptget' do
     meet{ aptget('libsasl2-dev')}
-    met?{ aptget('libsasl2-dev') =~ /.*libsasl2-dev is already the newest version.*/ }
+    met?{ aptget('libsasl2-dev') =~ /.*is already the newest version.*/ }
 end 
 
 dep 'libxsltdev.aptget' do
     meet{ aptget('libxslt-dev')}
-    met?{ aptget('libxslt-dev') =~ /.*libxslt1-dev is already the newest version.*/ }
+    met?{ aptget('libxslt-dev') =~ /.*is already the newest version.*/ }
 end 
 
 dep 'libxml2dev.aptget' do
     meet{ aptget('libxml2-dev')}
-    met?{ aptget('libxml2-dev') =~ /.*libxml2-dev is already the newest version.*/ }
+    met?{ aptget('libxml2-dev') =~ /.*is already the newest version.*/ }
 end 
 
 dep 'imagemagick.aptget' do
     meet{ aptget('imagemagick')}
-    met?{ aptget('imagemagick') =~ /.*imagemagick is already the newest version.*/ }
+    met?{ aptget('imagemagick') =~ /.*is already the newest version.*/ }
 end 
 
 dep 'libmagickcoredev.aptget' do
     meet{ aptget('libmagickcore-dev')}
-    met?{ aptget('libmagickcore-dev') =~ /.*libmagickcore-dev is already the newest version.*/ }
+    met?{ aptget('libmagickcore-dev') =~ /.*is already the newest version.*/ }
 end 
 
 dep 'libmagickwanddev.aptget' do
     meet{ aptget('libmagickwand-dev')}
-    met?{ aptget('libmagickwand-dev') =~ /.*libmagickwand-dev is already the newest version.*/ }
+    met?{ aptget('libmagickwand-dev') =~ /.*is already the newest version.*/ }
 end 
 
 dep 'libreadline_ruby.aptget' do
     meet { aptget('libreadline-ruby')}
-    met?{ aptget('libreadline-ruby') =~ /.*libreadline-ruby is already the newest version.*/ }
+    met?{ aptget('libreadline-ruby') =~ /.*is already the newest version.*/ }
 end
 
 dep 'libreadline5dev.aptget' do
     meet { aptget('libreadline5-dev')}
-    met?{ aptget('libreadline5-dev') =~ /.*libreadline5-dev is already the newest version.*/ }
+    met?{ aptget('libreadline5-dev') =~ /.*is already the newest version.*/ }
 end
 
 dep 'memcached.aptget' do 
 	meet { aptget('memcached') }
-	met?{ aptget('memcached') =~ /.*memcached is already the newest version.*/ }
+	met?{ aptget('memcached') =~ /.*is already the newest version.*/ }
 end
 
 
