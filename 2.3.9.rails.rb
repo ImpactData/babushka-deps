97f368856e0ac04747a55ba8a4542eb89368431d
Ben MacLeod <ben@impactdata.com.au>
2011-04-22 17:00:59 +1000

    DRY up Gems in Rails dep file

--- 2.3.9.rails.rb
+++ 2.3.9.rails.rb
@@ -6,50 +6,42 @@ end
 
 meta :aptget do
   def aptget args
-    sudo("apt-get --assume-yes install #{args}")
+    sudo "apt-get --assume-yes install #{args}"
   end
 end
 
 dep '2.3.9.rails' do
-    requires '2.3.9.rails.gem', 'bundler.gem', 'heroku.gem', 'thin.gem', 'postgres.gem', 'mongo.aptget' 
+  requires '2.3.9.rails.gem', 'bundler.gem', 'heroku.gem', 'thin.gem', 'postgres.gem', 'mongo.aptget'
 end
 
 dep '2.3.9.rails.gem' do
-    met? { gem('list rails') =~ /.*rails \(2\.3\.9\).*/ }
-    meet { gem('install rails --VERSION=2.3.9 --include-dependencies') }
-end 
+  met? { gem('list rails') =~ /.*rails \(2\.3\.9\).*/ }
+  meet { gem('install rails --VERSION=2.3.9 --include-dependencies') }
+end
 
 dep 'postgres.gem' do
-    requires 'postgres.access'
-    met? { gem('list postgres')['postgres'] }
-    meet { gem('install postgres') }
+  requires 'postgres.access'
+  met? { gem('list postgres')['postgres'] }
+  meet { gem('install postgres') }
 end
 
 dep 'bundler.gem' do
-    requires 'rubygems_update.gem'
-    met? { gem('list bundler')['bundler'] }
-    meet { 
-	log "bundler is not a happy camper"
-	log "you may have to do this manually"
-	log "rvm use ree-1.8.7-2011.03"
-	log "gem install bundler"
-	shell('rvm use ree-1.8.7-2011.03')
-	shell('gem install bundler') 
-    }
+  requires 'rubygems-update.gem'
+  met? { gem('list bundler')['bundler'] }
+  meet {
+    log "bundler is not a happy camper"
+    log "you may have to do this manually"
+    log "rvm use ree-1.8.7-2011.03"
+    log "gem install bundler"
+    shell('rvm use ree-1.8.7-2011.03')
+    shell('gem install bundler')
+  }
 end
 
-dep 'rubygems_update.gem' do
-    met? { gem('list rubygems-update')['rubygems-update'] }
-    meet { gem('install rubygems-update') }
-end
-
-dep 'heroku.gem' do
-    met? { gem('list heroku')['heroku'] }
-    meet { gem('install heroku') }
-end
-
-dep 'thin.gem' do
-    met? { gem('list thin')['thin'] }   
-    meet { gem('install thin') }
+GEMS = ['rubygems-update', 'heroku', 'thin'].each do |gem|
+  dep "#{gem}.gem" do
+    met? { gem("list #{gem}")[gem] }
+    meet { gem("install #{gem}") }
+  end
 end
 
