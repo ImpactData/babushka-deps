676fe2ef200a97ad488452295c2ea31f583785db
MarkMagnus <mark@impactdata.com.au>
2011-08-11 14:24:58 +1000

    Edited heroku_drains_rsyslog.rb via GitHub

--- heroku_drains_rsyslog.rb
+++ heroku_drains_rsyslog.rb
@@ -1,4 +1,9 @@
 dep 'rsyslog.aptget' do
+    log("vi /etc/rsyslog.conf")
+    log("uncomment $ModLoad imtcp")
+    log("uncomment $InputTCPServerRun 514")
+    log("services rsyslog stop")
+    log("services rsyslog start")
     meet { aptget('rsyslog') }
     met? { aptget('rsyslog') =~ /.*rsyslog is already the newest version.*/ }
 end
