96ba6b322e3a4460fd9da0f4416988cad33fad05
MarkMagnus <mark@impactdata.com.au>
2011-08-11 15:58:29 +1000

    Edited sb_sync.sh via GitHub

--- sb_sync.sh
+++ sb_sync.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-#restore mongo database
-mongorestore --db torsion-production /files/mongo/app438149
-#restore postres database
-pg_restore --verbose --clean --no-acl --no-owner --no-password -h 127.0.0.1 -U torsion -d torsion /files/postgresql/freshest.dump
 #checkout production version of squawkbox  
 rm -rf Squawkbox
 mkdir Squawkbox
@@ -14,7 +10,3 @@ chmod 777 -R ~/Squawkbox/log
 #restart all services
 sudo service nginx stop
 sudo service nginx start
-sudo service postgresql stop
-sudo service postgresql start
-sudo service mongodb stop
-sudo service mongodb start
