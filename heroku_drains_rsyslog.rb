5e90ed6df7e3593d353f16cceb579c5f1d9cf233
MarkMagnus <mark@impactdata.com.au>
2011-08-11 14:54:57 +1000

    Edited heroku_drains_rsyslog.rb via GitHub

--- heroku_drains_rsyslog.rb
+++ heroku_drains_rsyslog.rb
@@ -2,6 +2,13 @@ dep 'rsyslog.aptget' do
     log("vi /etc/rsyslog.conf")
     log("uncomment $ModLoad imtcp")
     log("uncomment $InputTCPServerRun 514")
+    log("change $FileOwner adm")
+    log("change $FileGroup adm")
+    log("change $FileCreateMode 0640")
+    log("change $DirCreateMode 0755")
+    log("change $Umask 0022")
+    log("change $PrivDropToUser adm")
+    log("change $PrivDropToGroup adm")
     log("services rsyslog stop")
     log("services rsyslog start")
     meet { aptget('rsyslog') }
