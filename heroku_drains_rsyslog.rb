d36acc95ef81b9fd0be5ff2114c3ed1869f7df92
MarkMagnus <mark@impactdata.com.au>
2011-08-11 14:14:27 +1000

    Edited heroku_drains_rsyslog.rb via GitHub

--- heroku_drains_rsyslog.rb
+++ heroku_drains_rsyslog.rb
@@ -1,4 +1,4 @@
 dep 'rsyslog.aptget' do
     meet { aptget('rsyslog') }
-    met? { aptget('rsyslog') =~ /.*bison is already the newest version.*/ }
+    met? { aptget('rsyslog') =~ /.*rsyslog is already the newest version.*/ }
 end
