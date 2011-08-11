4b6b3b4b68e3955f6eff2639fe3dafd6b229ef85
MarkMagnus <mark@impactdata.com.au>
2011-08-11 17:21:45 +1000

    Edited sb_sync.sh via GitHub

--- sb_sync.sh
+++ sb_sync.sh
@@ -7,5 +7,5 @@ git clone git@github.com:ImpactData/Squawkbox.git /var/www/squawkbox
 echo "rvm use ree" > /var/www/squawkbox/.rvmrc
 cd /var/www/squawkbox/ && git checkout production
 cd /var/www/squawkbox/ && rvm use ree && bundle install
-chmod 777 -R /var/www/squawkbox/Squawkbox/log
+chmod 777 -R /var/www/squawkbox/log
 sudo service nginx start
