a447350193eb524b9fc9f6239f55b3f4dfc94039
MarkMagnus <mark@impactdata.com.au>
2011-08-11 11:41:51 +1000

    Edited id_ruby_environment.sh via GitHub

--- id_ruby_environment.sh
+++ id_ruby_environment.sh
@@ -29,20 +29,12 @@ babushka 'impactdata:1.8.7.ee.rvm'
 #Rails 2.3.9 + gems[bundler, heroku, thin, postgres, pgadmin3, mongodb]
 babushka 'impactdata:2.3.9.rails.rvm'
 
-#git clone git@github.com:ImpactData/Squawkbox.git
-#cd Squawkbox
-#git checkout development
-
-#bundle install # installs gems from SB Gemfile
-
-# Not sure all of these are necessary
+#If you want to checkout sb
+#babushka 'impactdata:squawkbox.setup'
 #rake db:create
-#rake db:create test
 #rake db:migrate
-#rake db:migrate test
 #rake db:seed
-#rake db:seed test
-babushka 'impactdata:squawkbox.setup'
+
 
 
 
