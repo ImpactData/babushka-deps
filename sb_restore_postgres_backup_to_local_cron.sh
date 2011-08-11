fd0a26097153dd6b8bdfec0457e88f6fe7e6b878
MarkMagnus <mark@impactdata.com.au>
2011-08-11 16:18:29 +1000

    Edited sb_restore_postgres_backup_to_local_cron.sh via GitHub

--- sb_restore_postgres_backup_to_local_cron.sh
+++ sb_restore_postgres_backup_to_local_cron.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #restore postres database
-pg_restore --verbose --clean --no-acl --no-owner --no-password -h 127.0.0.1 -U torsion -d torsion /backup/postgresql/freshest.dump
+#requires a ~/.pgpass file 
+#containing *:*:*:*:torsion
+sudo pg_restore --verbose --clean --no-acl --no-owner --no-password -h 127.0.0.1 -U torsion -d torsion backup/postgresql/freshest.dump
 sudo service postgresql stop
 sudo service postgresql start
