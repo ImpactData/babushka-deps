9e2b3c849e2661236a6a80b31a6b43274910352e
MarkMagnus <mark@impactdata.com.au>
2011-08-12 10:20:22 +1000

    redo changes thanks git

--- sb_backup_postgres_cron.sh
+++ sb_backup_postgres_cron.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 #initiate heroku backup
-rvm use ree
-heroku pgbackups:capture --expire --app squawkbox
+/usr/local/rvm/bin/rvm use ree && heroku pgbackups:capture --expire --app squawkbox
 #get postgres backup files
 curl -o /backup/postgresql/freshest.dump `heroku pgbackups:url --app squawkbox`
