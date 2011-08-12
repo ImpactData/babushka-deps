b01b6e7a6c5d6e1e852f3c3864edbbb1f97cca98
MarkMagnus <mark@impactdata.com.au>
2011-08-12 10:05:00 +1000

    fuck off

--- sb_sync.sh
+++ sb_sync.sh
@@ -6,6 +6,6 @@ mkdir /var/www/squawkbox
 git clone git@github.com:ImpactData/Squawkbox.git /var/www/squawkbox
 echo "rvm use ree" > /var/www/squawkbox/.rvmrc
 cd /var/www/squawkbox/ && git checkout production
-cd /var/www/squawkbox/ && rvm use ree && bundle install
-chmod 777 -R /var/www/squawkbox/Squawkbox/log
+cd /var/www/squawkbox/ && /usr/local/rvm/bin/rvm use ree && /usr/local/rvm/gems/ree*/bin/bundle install
+chmod 777 -R /var/www/squawkbox/log
 sudo service nginx start
