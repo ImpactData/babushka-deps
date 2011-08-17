5b4a60d2b62df4b7992706d63ac7466e81183df0
MarkMagnus <mark@impactdata.com.au>
2011-08-17 10:45:39 +1000

    finally fixed heroku troubles

--- sb_backup_postgres_cron.sh
+++ sb_backup_postgres_cron.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 #initiate heroku backup
-/usr/local/rvm/bin/rvm use ree 
-ruby -rubygems /usr/local/rvm/gems/ree-1.8.7-2011.03/bin/heroku pgbackups:capture --expire --app squawkbox
+/usr/local/rvm/scripts/rvm
+export PATH=$PATH:/usr/local/rvm/bin:/usr/local/rvm/gems/ree-1.8.7-2011.03/bin
+echo $PATH
+rvm use ree
+heroku pgbackups:capture --expire --app squawkbox
 #get postgres backup files
-HEROKU_PG_URL=`ruby -rubygems /usr/local/rvm/gems/ree-1.8.7-2011.03/bin/heroku pgbackups:url --app squawkbox`
+HEROKU_PG_URL=`heroku pgbackups:url --app squawkbox`
 echo $HEROKU_PG_URL
 curl -o /backup/postgresql/freshest.dump $HEROKU_PG_URL
