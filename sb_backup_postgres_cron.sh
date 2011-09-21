23507d530fd72a838bb4700f87721818bc6ebfc7
MarkMagnus <mark@impactdata.com.au>
2011-09-21 10:41:16 +1000

    final commit from the node, setting paths seems to fix my heroku gem woes

--- sb_backup_postgres_cron.sh
+++ sb_backup_postgres_cron.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 #initiate heroku backup
-/usr/local/rvm/scripts/rvm
-export PATH=$PATH:/usr/local/rvm/bin:/usr/local/rvm/gems/ree-1.8.7-2011.03/bin
-echo $PATH
-rvm use ree
+export PATH=/usr/local/rvm/gems/ree-1.8.7-2011.03/bin:/usr/local/rvm/gems/ree-1.8.7-2011.03@global/bin:/usr/local/rvm/rubies/ree-1.8.7-2011.03/bin:/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
+export GEM_HOME=/usr/local/rvm/gems/ree-1.8.7-2011.03
+#/usr/local/rvm/scripts/rvm
+#echo $PATH
+#rvm use ree
 heroku pgbackups:capture --expire --app squawkbox
 #get postgres backup files
 HEROKU_PG_URL=`heroku pgbackups:url --app squawkbox`
--- sb_sync.sh
+++ sb_sync.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 #checkout production version of squawkbox  
-sudo service nginx stop
+#sudo service nginx stop
+export PATH=/usr/local/rvm/gems/ree-1.8.7-2011.03/bin:/usr/local/rvm/gems/ree-1.8.7-2011.03@global/bin:/usr/local/rvm/rubies/ree-1.8.7-2011.03/bin:/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
+export GEM_HOME=/usr/local/rvm/gems/ree-1.8.7-2011.03 
 rm -rf /var/www/squawkbox
 mkdir /var/www/squawkbox
 git clone git@github.com:ImpactData/Squawkbox.git /var/www/squawkbox
 echo "rvm use ree" > /var/www/squawkbox/.rvmrc
-cd /var/www/squawkbox/ && git checkout production
-cd /var/www/squawkbox/ && /usr/local/rvm/bin/rvm use ree && /usr/local/rvm/gems/ree*/bin/bundle install
+cd /var/www/squawkbox/ 
+git checkout production
+#rvm use ree
+#echo $PATH
+#echo $GEM_HOME
+bundle install
 chmod 777 -R /var/www/squawkbox/log
 mkdir /var/www/squawkbox/tmp
 chmod 777 -R /var/www/squawkbox/tmp
