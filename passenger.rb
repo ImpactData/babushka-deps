b7e1d1fbe831d6fc27a97f9f4509503b3329899b
MarkMagnus <mark@impactdata.com.au>
2012-05-21 16:43:54 +1000

    Update passenger.rb

--- passenger.rb
+++ passenger.rb
@@ -1,6 +1,6 @@
 #generating a self signed key
 #openssl genrsa -des3 -out server.key 1024
-#openssl req -key server.key -out server.csr
+#openssl req -new -key server.key -out server.csr
 #openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
 #cp server.key server.key.secure
 #openssl rsa -in server.key.secure -out server.key
