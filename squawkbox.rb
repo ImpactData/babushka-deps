c1b26328fbd3e823ff0277672d343ddfa414c7d9
MarkMagnus <mark@impactdata.com.au>
2012-06-01 11:53:48 +1000

    Update squawkbox notes after checkout, rake commands now out off date.

--- squawkbox.rb
+++ squawkbox.rb
@@ -7,10 +7,7 @@ dep 'squawkbox_setup' do
     requires 'squawkbox_bundle', 'rvmrc'    
     log "Upon successfully installation of sqauwkbox"
 	log "cd Squawkbox"
-    log "change host file to include; 0.0.0.0 development"
-    log "rake db:create"
-    log "rake db:migrate"
-    log "rake db:seed"
+    log "change hosts file to include; 0.0.0.0 development"
 end
 
 dep 'rvmrc' do 
